1b14124fd8a3d5f8e117bcd14e92b02d78bc39d0
Jakub Jankiewicz <jcubic@onet.pl>
2022-07-15 18:37:44 +0200

    fix build

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from "react";
-import * as ReactDOMClient from "react-dom/client";
+import ReactDOMClient from "react-dom/client";
 
 import App from "./components/App";
 
-const rootElement = document.getElementById("root");
+const rootElement = document.getElementById("root") as HTMLElement;
 const root = ReactDOMClient.createRoot(rootElement);
 
 root.render(
